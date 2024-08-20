6b34a988544bcdcae98271563f8a75a18857d77b
Torresito <nicolastorresroa@gmail.com>
2024-08-20 10:43:35 -0500

    Se agrega la función de visualizar datos, agregar libros y visualizar libros

--- adj.h
+++ adj.h
@@ -1,5 +1,7 @@
 #include <fstream>
 #include <iomanip>
+#include <iostream>
+#include <cstring>
 using namespace std;
 
 struct libro {
@@ -61,61 +63,142 @@ struct cant_l{
  int cant_devolución;
 };
 
-void menu (){
+void menu(l_devolucion* &dev, l_libros* &lib, l_prestamo* &pres, l_usuario* &usu, fstream*& in);
+void visualizar_datos(l_devolucion* &dev, l_libros* &lib, l_prestamo* &pres, l_usuario* &usu);
+void agregar_datos();
+void cargar_datos(l_devolucion* &dev, l_libros* &lib, l_prestamo* &pres, l_usuario &usu, fstream*& in);
+void guardar_datos(l_devolucion* &dev, l_libros* &lib, l_prestamo* &pres, l_usuario* &usu, fstream* &out);
+void agregar_libros(l_libros* &lib);
+void imprimir_libros(l_libros* inicial);
+
+void menu (l_devolucion* &dev, l_libros* &lib, l_prestamo* &pres, l_usuario* &usu, fstream*& in){
 int select = 5;
+do{
+    system("cls");
+    cout << setw(50)<< setfill('*') <<"*" << '\n';
+    cout << setw(35) << setfill(' ') << right << "Gestion de Biblioteca V0.1" << '\n';
+    cout << setw(50)<< setfill('*') <<"*" << "\n\n";
+    cout << "Bienvenido/a, elija alguna opción\n\n";
 
-cout << setw(50)<< setfill('*') <<"*" << '\n';
-cout << setw(35) << setfill(' ') << right << "Gestion de Biblioteca V0.1" << '\n';
-cout << setw(50)<< setfill('*') <<"*" << "\n\n";
-cout << "Bienvenido/a, elija alguna opción\n\n";
+    cout << "1. Visualizar Datos Generales\n";
+    cout << "2. Agregar Libros\n";
+    cout << "3. Registrar Usuarios\n";
+    cout << "4. Registrar Prestamo\n";
+    cout << "5. Salir\n\n";
 
-cout << "1. Visualizar Datos Generales\n";
-cout << "2. Agregar Libros\n";
-cout << "3. Registrar Usuarios\n";
-cout << "4. Registrar Prestamo\n";
-cout << "5. Salir\n\n";
+    cout << "Selección: ";
+    cin >> select;
+    cout << "\n\n";
 
-cout << "Selección: ";
-cin >> select;
-cout << "\n\n";
+    switch (select)
+    {
+    case 1:
+        visualizar_datos(dev, lib, pres, usu);
+        break;
 
-switch (select)
+    case 2:
+        agregar_libros(lib);
+        break;
+
+    case 3:
+        /* code */
+        break;
+
+    case 4:
+        /* code */
+        break;
+
+    case 5:
+        break;
+
+    default:
+        cout << "Opción invalida, ingrese otra opción\n";
+        break;
+    }
+}while(select !=5 );
+}
+
+void visualizar_datos(l_devolucion* &dev, l_libros* &lib, l_prestamo* &pres, l_usuario* &usu){
+    int select;
+    system("cls");
+    cout << "Ingrese que datos desea visualizar\n";
+    
+    cout << "1. Libros\n";
+    cout << "2. Usuarios\n";
+    cout << "3. Prestamos\n";
+    cout << "4. Devoluciones\n";
+    cout << "5. Cancelar\n\n";
+
+    cin >> select;
+    cout << "\n\n";
+
+    switch (select)
+    {
+    case 1:
+        imprimir_libros(lib);
+        break;
+
+    case 2:
+        /* code */
+        break;
+
+    case 3:
+        /* code */
+        break;
+
+    case 4:
+        /* code */
+        break;
+
+    case 5:
+        break;
+
+    default:
+        cout << "Opción invalida, ingrese otra opción\n";
+        break;
+    }
+}
+
+void agregar_libros(l_libros* &lib)
 {
-case 1:
-    /* code */
-    break;
+char select = 'n';
+do{
+    system("cls");
+    l_libros* aux = new l_libros;
+    cout << "Ingrese el Id del libro (5 caracteres)\n";
+    cin.ignore();
+    cin.getline(aux->libros.id, 5);
+    cout << "Ingrese el titulo del libro (30 caracteres)\n";
 
-case 2:
-    /* code */
-    break;
+    cin.getline(aux->libros.titulo, 29);
+    cout << "Ingrese el Autor del libro (25 caracteres)\n";
 
-case 3:
-    /* code */
-    break;
+    cin.getline(aux->libros.autor, 24);
+    cout << "Ingrese el año de publicación\n";
+    cin >> aux->libros.año_pub;
+    cout << "Ingrese la cantidad de copias\n";
+    cin >> aux->libros.cant_copias;
 
-case 4:
-    /* code */
-    break;
+    if(lib == nullptr)
+        {
+            lib = aux;	
+        }else{
+            l_libros* temp = lib;
+            while(temp->siguiente != nullptr)
+            {
+                temp = temp->siguiente;
+            }
+            temp->siguiente = aux;
+        }
 
-case 5:
-    break;
-
-default:
-    cout << "Opción invalida, ingrese otra opción\n";
-    break;
+    cout << "Libro añadido exitosamente!\n";
+    cout << "¿Desea añadir otro libro? (y/n)" << endl;
+    cin >> select;
+    select = tolower(select);
+}while(select != 'n');
 }
 
-}
-
-
-
-void agregar_datos()
-{
-
-}
-
-void cargar_datos(l_devolucion* &dev, l_libros* &lib, l_prestamo* &pres, l_usuario &usu, fstream*&
- in){
+void cargar_datos(l_devolucion* &dev, l_libros* &lib, l_prestamo* &pres, l_usuario &usu, fstream*& in){
 
 in->open("devolucion.dat", ios::in);
 
@@ -126,6 +209,8 @@ if (!in->fail())
     {
         l_devolucion* nuevo = new l_devolucion();
         in->read((char*) dev, sizeof(devolucion));
+
+
 
         dev = dev->siguiente; 
 
@@ -144,6 +229,18 @@ void guardar_datos(l_devolucion* &dev, l_libros* &lib, l_prestamo* &pres, l_usua
 
 void agregar_libros(){
 
+}
 
+void imprimir_datos(){
 
+}
+
+void imprimir_libros(l_libros* inicial) {
+    l_libros* temp = inicial;
+    while (temp != nullptr) {
+        cout<<temp->libros.id << " " << temp->libros.titulo << " " << temp->libros.autor << '\n';
+        temp = temp->siguiente;
+    }
+    cout<<endl;
+    system("pause");
 }
--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
+#include <cstring>
 #include "adj.h"
 
 using namespace std;
-fstream archivo;
+fstream* archivo;
 
 l_libros* libros_gen = nullptr;
 l_usuario* usuarios_gen = nullptr;
@@ -14,7 +15,7 @@ l_devolucion* devoluciones_gen = nullptr;
 int main ()
 {
 
-menu();
+menu(devoluciones_gen, libros_gen, prestamos_gen, usuarios_gen, archivo);
     
     return 0;
 }
